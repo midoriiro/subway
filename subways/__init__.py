be9db8b9e876f311957c72a70a659b84618e46e5
midoriiro <alexis@bekhdadi.com>
2016-10-13 14:15:44 +0200

    change module name to subways, subway is already exist in pypi.

---
+++ subways/__init__.py
@@ -0,0 +1,6 @@
+'''
+Subway provide some basic utilities like Factory objects, Process management
+based on subprocess module, some class to manipulate dictionaries and more.
+'''
+
+__version__ = 0.1

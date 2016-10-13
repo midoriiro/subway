cee33688de80b7fc90140f98bb7354ca57cea60b
midoriiro <alexis@bekhdadi.com>
2016-10-13 16:42:55 +0200

    Update setup.py with correct README.md, LICENSE, MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
+import os
 
 import subways
 
+
+def get_readme():
+    return open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
 setup(
-    name='subway',
+    name='subways',
     version=subways.__version__,
-    packages=['subways'],
-    url='',
-    license='',
+    packages=find_packages(),
+    url='https://github.com/midoriiro/subways/',
+    license=open('LICENSE').read(),
     author='midoriiro',
     author_email='contact@smartsoftwa.re',
-    description='',
-    install_requires=[]
+    maintainer='midoriiro',
+    maintainer_email='contact@smartsoftwa.re',
+    description=subways.__doc__,
+    long_description=open('README.md').read(),
+    tests_require=['tox'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries'
+    ],
 )
--- subways/__init__.py
+++ subways/__init__.py
@@ -3,4 +3,4 @@ Subway provide some basic utilities like Factory objects, Process management
 based on subprocess module, some class to manipulate dictionaries and more.
 '''
 
-__version__ = 0.1
+__version__ = '0.1.0'
